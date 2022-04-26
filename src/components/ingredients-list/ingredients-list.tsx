ce9ff103e6b2957f1984dec50dc6c3b174b995cc
Sergei Anokhin <sa.anokhin@yandex.ru>
2022-04-26 19:19:18 +0300

    Удалил лишние файлы

--- src/components/ingredients-list/ingredients-list.tsx
+++ src/components/ingredients-list/ingredients-list.tsx
@@ -6,10 +6,10 @@ function IngredientsList(props: any) {
     const type = props.type;
     return (
         <>
-        <h2 className="text text_type_main-medium mb-6" id={props.id}>{props.name}</h2>
-        <div className={`${styles.items} pl-4 pr-4 mb-10`}>
-        {data.map((elem, index) => elem.type === type && <Item key={index} image={elem.image} price={elem.price} name={elem.name} />)}
-        </div>
+            <h2 className="text text_type_main-medium mb-6" id={props.id}>{props.name}</h2>
+            <div className={`${styles.items} pl-4 pr-4 mb-10`}>
+                {data.map((elem, index) => elem.type === type && <Item key={index} image={elem.image} price={elem.price} name={elem.name} />)}
+            </div>
         </>
     );
 }
