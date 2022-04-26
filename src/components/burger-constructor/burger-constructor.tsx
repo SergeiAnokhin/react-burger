75475b07ee5b26d4e8f14ea785eb682a6b534f11
Sergei Anokhin <sa.anokhin@yandex.ru>
2022-04-27 00:02:11 +0300

    Правки по блоку конструктора

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,9 +1,56 @@
-import './burger-constructor.css';
+import styles from './burger-constructor.module.css';
+import { ConstructorElement, DragIcon, CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components';
+import data from '../../utils/data';
 
 function BurgerConstructor() {
   return (
-      <section className="section pt-10">
-        <h2>Конструктор</h2>
+      <section className={`${styles.section} pt-25 pl-4 pr-4`}>
+        <div className={styles.xxx}>
+          <ConstructorElement
+            type="top"
+            isLocked={true}
+            text="Краторная булка N-200i (верх)"
+            price={200}
+            thumbnail='https://code.s3.yandex.net/react/code/bun-02-mobile.png'
+          />
+        </div>
+        <div className={styles.elements}>
+          {data.map((elem, index) =>
+          elem.type === 'main' ?
+          <div style={{display: 'flex', alignItems: 'center', gap: '8px', marginLeft: '16px'}}>
+            <DragIcon type="primary" />
+            <ConstructorElement 
+              key={index}
+              text={elem.name} 
+              price={elem.price} 
+              thumbnail={elem.image_mobile}
+            />
+          </div> :
+          elem.type === 'sauce' &&
+          <div style={{display: 'flex', alignItems: 'center', gap: '8px', marginLeft: '16px'}}>
+            <DragIcon type="primary" />
+            <ConstructorElement 
+              key={index}
+              text={elem.name} 
+              price={elem.price} 
+              thumbnail={elem.image_mobile}
+            />
+          </div>
+          )}
+        </div>
+        <div className={styles.xxx}>
+          <ConstructorElement
+            type="bottom"
+            isLocked={true}
+            text="Краторная булка N-200i (низ)"
+            price={200}
+            thumbnail='https://code.s3.yandex.net/react/code/bun-02-mobile.png'
+          />
+        </div>
+        <div className={`${styles.info} pr-4`}>
+          <p className="text text_type_digits-medium mr-10"><span className='mr-2'>610</span><CurrencyIcon type="primary"/></p>
+          <Button type="primary" size="large">Оформить заказ</Button>
+        </div>
       </section>
   );
 }
