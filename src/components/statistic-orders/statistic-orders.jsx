5d74212f15b5c4ec4110471072743f3bd171670d
Sergei Anokhin <sa.anokhin@yandex.ru>
2022-07-04 20:22:37 +0300

    Доработал статусы в ленте заказов

--- src/components/statistic-orders/statistic-orders.jsx
+++ src/components/statistic-orders/statistic-orders.jsx
@@ -1,32 +1,44 @@
+import { nanoid } from 'nanoid';
 import { useSelector } from 'react-redux';
 import styles from './statistic-orders.module.css';
 
 export const StatisticOrders = () => {
-  const { total, totalToday } = useSelector((store) => store.wsReducer);
+  const { total, totalToday, orders } = useSelector((store) => store.wsReducer);
+
   return (
     <div className={styles.statistic}>
       <div className={styles.statuses}>
         <div className={styles.status}>
           <h2 className={styles.title}>Готовы:</h2>
           <ul className={styles.list}>
-            <li className={`${styles.item} ${styles.ready}`}>034533</li>
-            <li className={`${styles.item} ${styles.ready}`}>034533</li>
-            <li className={`${styles.item} ${styles.ready}`}>034533</li>
-            <li className={`${styles.item} ${styles.ready}`}>034533</li>
-            <li className={`${styles.item} ${styles.ready}`}>034533</li>
-            <li className={`${styles.item} ${styles.ready}`}>034533</li>
-            <li className={`${styles.item} ${styles.ready}`}>034533</li>
+            {orders
+              .filter((item) => item.status === 'done')
+              .map(
+                (elem, i) =>
+                  i < 10 && (
+                    <li
+                      key={nanoid()}
+                      className={`${styles.item} ${styles.ready}`}
+                    >
+                      {elem.number}
+                    </li>
+                  )
+              )}
           </ul>
         </div>
         <div className={styles.status}>
           <h2 className={styles.title}>В работе:</h2>
           <ul className={styles.list}>
-            <li className={styles.item}>034533</li>
-            <li className={styles.item}>034533</li>
-            <li className={styles.item}>034533</li>
-            <li className={styles.item}>034533</li>
-            <li className={styles.item}>034533</li>
-            <li className={styles.item}>034533</li>
+            {orders
+              .filter((item) => item.status !== 'done')
+              .map(
+                (item, i) =>
+                  i < 10 && (
+                    <li key={nanoid()} className={styles.item}>
+                      {item.number}
+                    </li>
+                  )
+              )}
           </ul>
         </div>
       </div>
